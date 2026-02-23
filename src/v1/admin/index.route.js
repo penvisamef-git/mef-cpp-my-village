65116677016b9d809c6d024597dda89672fc2d95
One EV KH <oneevkh@gmail.com>
2026-02-23 19:10:30 +0700

    11th

--- src/v1/admin/index.route.js
+++ src/v1/admin/index.route.js
@@ -4,11 +4,11 @@ const index = (prop) => {
 
   // All Route ==========================
   const authRoute = require("./auth/auth.route");
-  // const sessionRoute = require("./session/session.route");
-  // const userRoute = require("./user/user.route");
-  // const userGroupRoute = require("./user/group/group_user.route");
-  // const activityLogRoute = require("./activity_log/activity_log.route");
-  // const activityLogCategoryRoute = require("./activity_log_category/activity_log_category.route");
+  const sessionRoute = require("./session/session.route");
+  const userRoute = require("./user/user.route");
+  const userGroupRoute = require("./user/group/group_user.route");
+  const activityLogRoute = require("./activity_log/activity_log.route");
+  const activityLogCategoryRoute = require("./activity_log_category/activity_log_category.route");
   // const masterData_JobType = require("./dashboard/master_data/job/job_type/job_type.route");
   // const masterData_Job = require("./dashboard/master_data/job/job_name/job.route");
   // const masterData_EducationType = require("./dashboard/master_data/education/education_type/education_type.route");
@@ -36,11 +36,11 @@ const index = (prop) => {
   // const Temp = require("../../temp/temp.route");
   // Implement ==========================
   authRoute(prop); // Auth
-  // sessionRoute(prop); // Auth
-  // userRoute(prop); // User
-  // userGroupRoute(prop); // Group Permission
-  // activityLogRoute(prop); // Log
-  // activityLogCategoryRoute(prop); // Log
+  sessionRoute(prop); // Auth
+  userRoute(prop); // User
+  userGroupRoute(prop); // Group Permission
+  activityLogRoute(prop); // Log
+  activityLogCategoryRoute(prop); // Log
   // masterData_Job(prop);
   // masterData_EducationType(prop);
   // masterData_EducationLevel(prop);
