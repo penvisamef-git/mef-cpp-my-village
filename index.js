3f33d0b0b3b2c6092227b55b3e4f6df606803f80
One EV KH <oneevkh@gmail.com>
2026-02-23 18:31:32 +0700

    First Initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World 🌍");
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    message: "This is About Page",
+  });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
